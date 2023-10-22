fc54581690bc7f82dea3549fad5108d9d2edd575
aruZeta <me@aruzeta.com>
2023-10-22 15:47:37 +0200

    feat: flakez init command

--- commands.nix
+++ commands.nix
@@ -1,15 +1,32 @@
 { repo
 }:
 
-[
-  { name = "use";
-    scriptName = "flakez-use";
-    script = ''
+let
+  utils = {
+    paramsAsPath = ''
       IFS_BAK="$IFS"
       IFS='/'
       FLAKEZ_PATH="$*"
       IFS="$IFS_BAK"
+    '';
+  };
+in
+
+[
+  { name = "use";
+    scriptName = "flakez-use";
+    script = ''
+      ${utils.paramsAsPath}
       echo "use flake \"${repo}?dir=$FLAKEZ_PATH\"" >> .envrc
+      direnv allow
+    '';
+  }
+
+  { name = "init";
+    scriptName = "flakez-init";
+    script = ''
+      ${utils.paramsAsPath}
+      nix flake init -t "${repo}?dir=$FLAKEZ_PATH"
       direnv allow
     '';
   }

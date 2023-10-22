adc4d618e0a0181abcf24038913fde302d6d6d2f
aruZeta <me@aruzeta.com>
2023-10-22 15:19:52 +0200

    feat: flakez use command

--- commands.nix
+++ commands.nix
@@ -2,4 +2,15 @@
 }:
 
 [
+  { name = "use";
+    scriptName = "flakez-use";
+    script = ''
+      IFS_BAK="$IFS"
+      IFS='/'
+      FLAKEZ_PATH="$*"
+      IFS="$IFS_BAK"
+      echo "use flake \"${repo}?dir=$FLAKEZ_PATH\"" >> .envrc
+      direnv allow
+    '';
+  }
 ]

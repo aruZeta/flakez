42318c95a2e248a15a287994b2ba80eb286f4aa6
aruZeta <me@aruzeta.com>
2023-11-06 21:05:34 +0100

    feat: java 21

---
+++ java/21/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "flakez - Java 21";
+
+  inputs = {
+    # Set this to the actual nixpkgs you are going to use
+    nixpkgs.url = "github:NixOS/nixpkgs/82c294f13af5b364083f6043a6b0ac8a4581cfc2";
+
+    javaPkgs.url = "github:NixOS/nixpkgs/82c294f13af5b364083f6043a6b0ac8a4581cfc2";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , javaPkgs
+    , flake-utils
+    } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        java = javaPkgs.legacyPackages.${system}.jdk21;
+      in {
+        defaultPackage = java;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            java
+          ];
+
+          shellHook = ''
+            ${java}/bin/java --version
+          '';
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
